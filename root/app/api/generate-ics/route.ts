010e2c6b7be14132bb0b6b626b5f76ca9c6ab9e8
brandon <ramirezbrandon077@gmail.com>
2023-11-27 16:00:40 -0700

    removed generate-ics

--- root/app/api/generate-ics/route.ts
+++ root/app/api/generate-ics/route.ts
@@ -4,33 +4,33 @@
  *
  */
 
-import type { NextApiRequest, NextApiResponse } from "next";
-import ical from "ical-generator";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import ical from "ical-generator";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const startDateTime = Array.isArray(req.query.startDateTime)
-    ? req.query.startDateTime[0]
-    : req.query.startDateTime;
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const startDateTime = Array.isArray(req.query.startDateTime)
+//     ? req.query.startDateTime[0]
+//     : req.query.startDateTime;
 
-  const clientName = Array.isArray(req.query.clientName)
-    ? req.query.clientName[0]
-    : req.query.clientName;
+//   const clientName = Array.isArray(req.query.clientName)
+//     ? req.query.clientName[0]
+//     : req.query.clientName;
 
-  if (typeof startDateTime !== "string" || !Date.parse(startDateTime)) {
-    res.status(400).send("Invalid start date time");
-    return;
-  }
+//   if (typeof startDateTime !== "string" || !Date.parse(startDateTime)) {
+//     res.status(400).send("Invalid start date time");
+//     return;
+//   }
 
-  const endDateTime = new Date(new Date(startDateTime).getTime() + 30 * 60000); // 30 minutes later
+//   const endDateTime = new Date(new Date(startDateTime).getTime() + 30 * 60000); // 30 minutes later
 
-  const calendar = ical({ name: "Haircut with Sergio" });
-  calendar.createEvent({
-    start: new Date(startDateTime),
-    end: new Date(endDateTime),
-    summary: `Appointment with Sergio`,
-    description: "Your upcoming appointment.",
-  });
+//   const calendar = ical({ name: "Haircut with Sergio" });
+//   calendar.createEvent({
+//     start: new Date(startDateTime),
+//     end: new Date(endDateTime),
+//     summary: `Appointment with Sergio`,
+//     description: "Your upcoming appointment.",
+//   });
 
-  res.setHeader("Content-Type", "text/calendar");
-  res.send(calendar.toString());
-}
+//   res.setHeader("Content-Type", "text/calendar");
+//   res.send(calendar.toString());
+// }
