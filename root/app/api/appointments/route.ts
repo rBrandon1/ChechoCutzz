154c785bbf4ca08ebc56265cb108662af9126534
brandon <ramirezbrandon077@gmail.com>
2023-11-28 00:55:31 -0700

    switched nodemailer to port 465 from 587

--- root/app/api/appointments/route.ts
+++ root/app/api/appointments/route.ts
@@ -252,8 +252,8 @@ export async function DELETE(req: NextRequest) {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.zoho.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.ZOHO_EMAIL,
         pass: process.env.ZOHO_PASSWORD,
