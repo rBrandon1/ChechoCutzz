8c0501cba742842d6cd183d57ec0648bb71f087a
brandon <ramirezbrandon077@gmail.com>
2024-02-05 22:25:42 -0700

    figuring out the issue

--- root/lib/supabase/server.ts
+++ root/lib/supabase/server.ts
@@ -4,7 +4,7 @@ import { NextApiRequest } from "next";
 export function createSupabase(req: NextApiRequest) {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       global: {
         headers: {
