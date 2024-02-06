f22d68a059af6d4121c1bb524207f57f1b64e589
brandon <ramirezbrandon077@gmail.com>
2024-02-05 22:49:58 -0700

    Potential fix

--- root/lib/supabase/server.ts
+++ root/lib/supabase/server.ts
@@ -3,8 +3,8 @@ import { NextApiRequest } from "next";
 
 export function createSupabase(req: NextApiRequest) {
   return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       global: {
         headers: {
